1a6d5bbb841984fa819e477eefe6e18abdedf0a1
Priscilla <102761223+PriscillaBakradze@users.noreply.github.com>
2022-05-17 23:19:06 -0400

    Create fcc18.js

---
+++ FreeCodeCamp/fcc18.js
@@ -0,0 +1,25 @@
+//Debugging
+
+//1. Use the console.log() method to print the value of the variable a where noted in the code.
+let a = 5;
+let b = 1;
+a++;
+
+console.log(a);
+
+let sumAB = a + b;
+console.log(sumAB);
+
+//2. First, open your browser console so you can see the logs. To do that, you can right-click the freeCodeCamp navigation bar at the top and click inspect on most browsers. Then find the console tab in the window that opens. 
+//After that, use console.log to log the output variable. View the two consoles to see the log. Finally, use console.clear after your log to clear the browser console. View the difference in the two consoles.
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
+
+//3. Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+
+console.log(typeof seven);
+console.log(typeof three);
